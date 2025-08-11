3c6ec2f0d14db097e5864ca9ffa7bfbf3e6bdb20
Risca Victor <victor.risca01@gmail.com>
2025-08-11 19:07:17 +0300

    Test adaugare produs

--- shop-backend/src/main/resources/data.sql
+++ shop-backend/src/main/resources/data.sql
@@ -5,4 +5,5 @@ VALUES ('88', 'Alfred', 'https://example.com/avatar.jpg', 4000);
 INSERT INTO reward (rewardid, name, description, full_description, price, image, category, in_stock, stock_count)
 VALUES 
 ('1', 'Steam Card', 'Steam Gift Card', 'A 50$ Steam gift card', 50.0, 'https/examplu.com/img1.jpg', 'gaming', true, 10),
-('11', 'Spotify Premium', 'Spotify Subscription', '3-month Spotify subscription', 30.0, 'https/examplu.com/img2.jpg', 'music', true, 5);
+('11', 'Spotify Premium', 'Spotify Subscription', '3-month Spotify subscription', 30.0, 'https/examplu.com/img2.jpg', 'music', true, 5),
+('2', 'Apple Card', 'Apple Gift Card', 'A 50$ Apple gift card', 50.0, 'https/examplu.com/img3.jpg', 'voucher', true, 10);
