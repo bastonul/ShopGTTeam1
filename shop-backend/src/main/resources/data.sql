4643d5a9cbfa45010e89578d0bfd236195966235
Risca Victor <victor.risca01@gmail.com>
2025-08-12 18:16:02 +0300

    Update stoc fizic

--- shop-backend/src/main/resources/data.sql
+++ shop-backend/src/main/resources/data.sql
@@ -4,6 +4,9 @@ VALUES ('88', 'Alfred', 'https://example.com/avatar.jpg', 4000);
 
 INSERT INTO reward (rewardid, name, description, full_description, price, image, category, in_stock, stock_count)
 VALUES 
-('1', 'Steam Card', 'Steam Gift Card', 'A 50$ Steam gift card', 50.0, 'https/examplu.com/img1.jpg', 'gaming', true, 10),
-('11', 'Spotify Premium', 'Spotify Subscription', '3-month Spotify subscription', 30.0, 'https/examplu.com/img2.jpg', 'music', true, 5),
-('2', 'Apple Card', 'Apple Gift Card', 'A 50$ Apple gift card', 50.0, 'https/examplu.com/img3.jpg', 'voucher', true, 10);
+('1', 'Tricou GenTech', 'Tricou GenTech Unisex', 'Tricou unisex, 100% bumbac, produs in Romania. Cu logo GeneratiaTech.', 500.0, 'https/examplu.com/img1.jpg', 'GenTech', 1, 5),
+('2', 'Cana GenTech', 'Cana GenTech', 'Perfecta pentru cafeaua de dimineata! O cana de 250ml, imprimata cu logo-ul  Generatia Tech', 200.0, 'https/examplu.com/img3.jpg', 'GenTech', 1, 12),
+('3', 'Stickere GenTech', 'Stickere GenTech', '50 de stickere Generatia Tech! Atat Logo-ul, Rob-Otto, si alte lucruri specifice GenTech!', 100.0, 'https/examplu.com/img2.jpg', 'GenTech', 1, 20),
+('4', 'Agenda GenTech', 'Agenda GenTech', 'Agenda 2026 cu GenTech.', 150.0, 'https/examplu.com/img1.jpg', 'GenTech', 1, 15),
+('5', 'Pix GenTech', 'Pix GenTech', 'Pix de culoare albastra Generatia Tech.', 100.0, 'https/examplu.com/img1.jpg', 'GenTech', 1, 40),
+('6', 'Rucsac GenTech', 'Steam Gift Card', 'Rucsac de drumetie Generatia Tech.', 400.0, 'https/examplu.com/img1.jpg', 'GenTech', 1, 5);
